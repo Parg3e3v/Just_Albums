2c47d836037c748dd69337e9845b81ce2032bf99
Craft3e3r <k.psa@mail.ru>
2022-03-12 21:01:17 +0400

    I don't believe it. IT WORKS. I mean... IT ACTUALLY WORKS! After 6 fail commits and a week of nothing I just wrote it! It's amazing

--- app/src/main/java/com/craft3r/JustAlbums/SettingsActivity.java
+++ app/src/main/java/com/craft3r/JustAlbums/SettingsActivity.java
@@ -9,17 +9,21 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.app.UiModeManager;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -34,11 +38,12 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.channels.FileChannel;
+import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    public String path = "/data/data/com.example.albumation/databases";
-    private static final int MY_PERMISSION_REQUEST_STORAGE = 1;
+    public String path = "/data/data/com.craft3r.JustAlbums/databases";
     ImageButton arrow;
     LinearLayout import_export, delete_db, dark_mode;
     public static final String SHARED_PREFS = "sharedPrefs";
@@ -50,7 +55,6 @@ public class SettingsActivity extends AppCompatActivity {
         editor.putString(TEXT, txt);
         editor.apply();
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,7 +95,6 @@ public class SettingsActivity extends AppCompatActivity {
                                 temp = txt[i];
                                 break;
                         }
-                        System.out.println(temp + "-------AA");
                         saveData(temp);
 
                     }
@@ -107,7 +110,6 @@ public class SettingsActivity extends AppCompatActivity {
                 AlertDialog alertDialog = builder.create();
                 alertDialog.setTitle("Set dark mode");
                 alertDialog.show();
-//                Toast.makeText(getApplicationContext(), "Not working yet :3", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -116,35 +118,38 @@ public class SettingsActivity extends AppCompatActivity {
         import_export.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Export not working yet :3", Toast.LENGTH_LONG).show();
+//                Toast.makeText(getApplicationContext(), "Export not working yet :3", Toast.LENGTH_LONG).show();
                 AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
                 String[] txt = {"Import", "Export"};
                 builder.setItems(txt, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        if (txt[i] == txt[0]){
+                        if (txt[0].equals(txt[i])) {
                             Intent intent = new Intent()
                                     .setType("*/*")
                                     .setAction(Intent.ACTION_GET_CONTENT);
 
                             startActivityForResult(Intent.createChooser(intent,
                                     "Select a file"), 123);
-                        }else if (txt[i] == txt[1]) {
+                        } else if (txt[1].equals(txt[i])) {
 
-                            if (ContextCompat.checkSelfPermission(SettingsActivity.this,
-                                    WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                                if (ActivityCompat.shouldShowRequestPermissionRationale(SettingsActivity.this,
-                                        WRITE_EXTERNAL_STORAGE))
-                                    ActivityCompat.requestPermissions(SettingsActivity.this, new String[]{
-                                            WRITE_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST_STORAGE);
-                                else
-                                    ActivityCompat.requestPermissions(SettingsActivity.this, new String[]{
-                                            WRITE_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST_STORAGE);
-                            }else {
-                                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-                                startActivityForResult(intent, 100);
-                                Toast.makeText(getApplicationContext(), "Export", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(Intent.ACTION_SEND);
+                            intent.setType("application/octet-stream");
+
+                            Uri uri = new FileProvider().getDatabaseURI(getApplicationContext());
+
+                            intent.putExtra(Intent.EXTRA_STREAM, uri);
+                            intent.putExtra(Intent.EXTRA_TEXT, "JustAlbums: top proga dlya top pacanov");
+                            List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+                            for (ResolveInfo resolveInfo : resInfoList) {
+                                String packageName = resolveInfo.activityInfo.packageName;
+                                grantUriPermission(packageName, uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
                             }
+                            startActivity(Intent.createChooser(intent, "Backup via:"));
+                        } else if (txt[2].equals(txt[i])) {
+                            Intent intent1 = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+                            startActivityForResult(Intent.createChooser(intent1,
+                                    "Select a folder"), 100);
                         }
                     }
                 })
@@ -221,56 +226,22 @@ public class SettingsActivity extends AppCompatActivity {
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case MY_PERMISSION_REQUEST_STORAGE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ContextCompat.checkSelfPermission(SettingsActivity.this,
-                            WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-                        startActivityForResult(intent, 100);
-                        Toast.makeText(getApplicationContext(), "Export", Toast.LENGTH_SHORT).show();
-                    }
-                } else
-                    Toast.makeText(this, "No permission granted!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == 123 && resultCode == RESULT_OK) {
             Uri selectedfile = data.getData();
-            //"file//" + selectedfile.getPath().split("/document/raw:")[1]
-//            String path = selectedfile.getPath().split("/document/raw:")[1];
-            if(copyFile(selectedfile, Uri.parse(path), true))
+            if(copyFile(selectedfile, Uri.parse(path), true)) {
                 Toast.makeText(getApplicationContext(), "Imported successfully",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }else
+                Toast.makeText(getApplicationContext(), "Import error",
                         Toast.LENGTH_SHORT).show();
 
         }
-        else if (requestCode == 100 && resultCode == RESULT_OK){
-            String ur = data.getData().getPath();
-            Uri pathToSave = Uri.parse(ur);
-            File file = new File(ur);
-            System.out.println(ur + "------AAAAA");
-            MediaScannerConnection.scanFile(this, new String[] { file.toString() }, null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                        public void onScanCompleted(String path, Uri uri) {
-                            Log.i("ExternalStorage", "Scanned " + path + ":");
-                            Log.i("ExternalStorage", "-> uri=" + uri);
-                        }
-                    });
-//pathToSave
-            File fl = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-            if (!fl.exists())
-                fl.mkdirs();
-            copyFile(Uri.parse("file://" + path + "/JustAlbums.db"), Uri.fromFile(
-                    fl), false);
-        }
     }
-
 
 
     // Copy Function
@@ -286,15 +257,6 @@ public class SettingsActivity extends AppCompatActivity {
         InputStream in = null;
         OutputStream out = null;
         try {
-
-            //create output directory if it doesn't exist
-            File dir = new File (outputPath.getPath());
-            if (!dir.exists())
-            {
-                dir.mkdirs();
-            }
-
-
             in = new FileInputStream(getApplicationContext().getContentResolver()
                     .openFileDescriptor(uri, "r").getFileDescriptor());
             try {
@@ -331,7 +293,7 @@ public class SettingsActivity extends AppCompatActivity {
         }
         catch (Exception e) {
             e.printStackTrace();
-            Toast.makeText(getApplicationContext(), "Can't import/export file",
+            Toast.makeText(getApplicationContext(), "Can't import file",
                     Toast.LENGTH_SHORT).show();
             return false;
         }
