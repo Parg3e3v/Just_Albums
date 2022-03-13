ed0bd39948d95626bb73fc130b01f9fb191aeaa4
Craft3e3r <k.psa@mail.ru>
2022-03-13 19:11:02 +0400

    Fixed bugs, deleted Albums.java

--- app/src/main/java/com/craft3r/JustAlbums/AlbumListFragment.java
+++ app/src/main/java/com/craft3r/JustAlbums/AlbumListFragment.java
@@ -9,19 +9,24 @@ import android.content.res.Configuration;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RatingBar;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -137,7 +142,7 @@ public class AlbumListFragment extends Fragment {
         contCont.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
             @Override
             public void onScrollChanged() {
-                if (contCont.getScrollY() > 10) {
+                if (contCont.getScrollY() > 650) {
                     MainActivity.to_top_but.setVisibility(View.VISIBLE);
                 }else {
                     MainActivity.to_top_but.setVisibility(View.GONE);
@@ -300,21 +305,19 @@ public class AlbumListFragment extends Fragment {
         }
 
             for (int i = 0; i < ids.size(); i++) {
-                Album alb = new Album(titles.get(i), artists.get(i), Reacts.get(i), IsEP.get(i),
-                        images.get(i), new float[]{r1.get(i), r2.get(i), r3.get(i)}, i+1,
-                        duration.get(i), counts.get(i), linkNames.get(i), links.get(i), year.get(i));
 
+                int id = i + 1;
                 int tempid_ = tempid + i;
 
-                alb.LoadAlbum(alb.id, alb.name, alb.artist, alb.IsEP, alb.img,
-                        alb.ratings, lay, view.getContext(), tempid_);
+                LoadAlbum(id, titles.get(i), artists.get(i), IsEP.get(i), images.get(i),
+                        new float[]{r1.get(i), r2.get(i), r3.get(i)}, lay, view.getContext(), tempid_);
 
                 LinearLayout example = view.findViewById(tempid_);
 
-                float rating = (alb.ratings[0] + alb.ratings[1] + alb.ratings[2]) / 3;
+                float rating = (r1.get(i) + r2.get(i) + r3.get(i)) / 3;
 
-                AlbumTupple temp = new AlbumTupple(example, i, alb.name, alb.artist, rating,
-                        alb.duration, alb.count, alb.year,
+                AlbumTupple temp = new AlbumTupple(example, i, titles.get(i), artists.get(i), rating,
+                        duration.get(i), counts.get(i), year.get(i),
                         (TextView) view.findViewById(tempid_ + tempid));
 
                 Albums.add(temp);
@@ -326,7 +329,7 @@ public class AlbumListFragment extends Fragment {
                     @Override
                     public void onClick(View view) {
                         Intent intent = new Intent(view.getContext(), EditActivity.class);
-                        intent.putExtra("id", String.valueOf(alb.id));
+                        intent.putExtra("id", String.valueOf(id));
                         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent);
                     }
@@ -455,5 +458,167 @@ public class AlbumListFragment extends Fragment {
                 // TODO Auto-generated method stub
             }
         });
+    }
+
+
+
+
+    public static void LoadAlbum(int id,String name, String artist, int IsEp,
+                                 Bitmap img, float[] ratings, LinearLayout lay, Context ct,
+                                 int idL){
+        // main container
+        // ----------------------------------------------------------------------------------------
+        LinearLayout example = new LinearLayout(ct);
+        example.setId(idL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(AlbumListFragment.dp(ct,10), 0, AlbumListFragment.dp(ct,10), AlbumListFragment.dp(ct,20));
+        example.setLayoutParams(params);
+        example.setOrientation(LinearLayout.HORIZONTAL);
+
+
+        // ----------------------------------------------------------------------------------------
+
+        // Id
+        // ----------------------------------------------------------------------------------------
+        TextView txt = new TextView(ct);
+        txt.setId(idL + 9999);
+
+        LinearLayout.LayoutParams txtParams =  new LinearLayout.LayoutParams(AlbumListFragment.dp(ct,20), AlbumListFragment.dp(ct,20));
+        txtParams.gravity = Gravity.CENTER_VERTICAL;
+        txtParams.setMargins(0, 0, AlbumListFragment.dp(ct,5), 0);
+        txt.setLayoutParams(txtParams);
+        txt.setGravity(Gravity.CENTER_VERTICAL);
+        txt.setText(String.valueOf(id));
+        txt.setTextSize(10);
+        txt.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        // ----------------------------------------------------------------------------------------
+
+        // Content
+        // ----------------------------------------------------------------------------------------
+        LinearLayout Cont = new LinearLayout(ct);
+        Cont.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        Cont.setOrientation(LinearLayout.HORIZONTAL);
+        // ----------------------------------------------------------------------------------------
+
+        // Image
+        // ----------------------------------------------------------------------------------------
+        ImageView im = new ImageView(ct);
+        im.setImageBitmap(img);
+        im.setLayoutParams(new ViewGroup.LayoutParams(AlbumListFragment.dp(ct,84),AlbumListFragment.dp(ct,84)));
+        Cont.addView(im);
+        // ----------------------------------------------------------------------------------------
+
+        // Info Layout
+        // ----------------------------------------------------------------------------------------
+        LinearLayout info = new LinearLayout(ct);
+        LinearLayout.LayoutParams inf = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        inf.weight = 1;
+        info.setOrientation(LinearLayout.VERTICAL);
+        info.setLayoutParams(inf);
+        Cont.addView(info);
+        // ----------------------------------------------------------------------------------------
+
+        // Name
+        // ----------------------------------------------------------------------------------------
+        LinearLayout l = new LinearLayout(ct);
+        LinearLayout.LayoutParams lParam = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        lParam.setMargins(AlbumListFragment.dp(ct,5), 0, 0, 0);
+        l.setLayoutParams(lParam);
+
+        l.setOrientation(LinearLayout.HORIZONTAL);
+        TextView nameT = new TextView(ct);
+
+
+        TextView EP = new TextView(ct);
+        if(IsEp == 1)
+            EP.setText("EP");
+
+        EP.setTextColor(Color.RED);
+
+        LinearLayout.LayoutParams EPP =  new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        EPP.gravity = Gravity.END;
+        EPP.weight = 0;
+        EPP.setMargins(AlbumListFragment.dp(ct,5), 0, 0, 0);
+        EP.setLayoutParams(EPP);
+        EP.setTextSize(16);
+
+
+        LinearLayout.LayoutParams nameP =  new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        nameP.weight = 0;
+
+        HorizontalScrollView horizScr = new HorizontalScrollView(ct);
+        ViewGroup.MarginLayoutParams horParam = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        horParam.setMarginStart(dp(ct, 5));
+        horizScr.setScrollBarSize(dp(ct, 1));
+        horizScr.setLayoutParams(horParam);
+
+        nameT.setLayoutParams(nameP);
+        nameT.setText(name);
+        nameT.setTextSize(16);
+
+        horizScr.addView(nameT);
+
+        l.addView(horizScr);
+        l.addView(EP);
+
+        info.addView(l);
+        System.out.println(name.length() + "---A");
+        if (name.length() > "miXXXtape II: Долгий путь".length()) {
+            ViewGroup.LayoutParams horParams = horizScr.getLayoutParams();
+            horParams.width = dp(ct, 220);
+            horizScr.setLayoutParams(horParams);
+        }
+        // ----------------------------------------------------------------------------------------
+
+        // Description
+        // ----------------------------------------------------------------------------------------
+
+        TextView desc = new TextView(ct);
+
+        LinearLayout.LayoutParams descP =  new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        descP.weight = 0;
+        desc.setLayoutParams(descP);
+        desc.setText(artist);
+        desc.setTextSize(12);
+
+        HorizontalScrollView horizScr2 = new HorizontalScrollView(ct);
+        ViewGroup.MarginLayoutParams horParam2 = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        horParam2.setMarginStart(dp(ct, 10));
+        horizScr2.setScrollBarSize(dp(ct, 1));
+        horizScr2.setLayoutParams(horParam2);
+
+        horizScr2.addView(desc);
+
+        info.addView(horizScr2);
+        // ----------------------------------------------------------------------------------------
+
+        // RatingBar
+        // ----------------------------------------------------------------------------------------
+        RatingBar rb = new RatingBar(ct,null, 0, R.style.Widget_App_CustomProgressBar);
+        rb.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                dp(ct,50)));
+        rb.setIsIndicator(true);
+        float rating = (ratings[0] + ratings[1] + ratings[2]) / 3;
+        rb.setRating(rating);
+        rb.setNumStars(5);
+        rb.setStepSize(0.5f);
+        rb.setScaleX(0.9f);
+        rb.setScaleY(0.9f);
+        rb.setPadding(0,0,0,0);
+
+        info.addView(rb);
+        // ----------------------------------------------------------------------------------------
+        example.addView(txt);
+        example.addView(Cont);
+        lay.addView(example);
+
     }
 }
